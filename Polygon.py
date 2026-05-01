078a35e6e3c79b83230d8745e6726ab35ae8e386
CoderVipra7 <vipratharwani7@gmail.com>
2026-05-01 17:44:17 +0530

    Add files via upload

---
+++ Polygon.py
@@ -0,0 +1,8 @@
+import turtle
+turtle.Screen().bgcolor("lavender")
+turtle.Screen().setup(500,500)
+polygon= turtle.Turtle()
+for i in range(6):
+    polygon.forward(100)
+    polygon.right(60)
+turtle.done()
